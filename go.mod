e43ee3cfd69deb923d6c47cce5ec115ee494f49f
dgoldstein1 <david.goldstein@deciphernow.com>
2019-12-17 09:54:50 -0500

    wrote first test..

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.12
 require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/gorilla/mux v1.7.3
+	github.com/stretchr/testify v1.4.0
 )
---
+++ server_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"os"
+	"testing"
+)
+
+var logFatal = log.Fatalf
+
+func TestReadInConfig(t *testing.T) {
+	originLogFatalf := logFatal
+	defer func() { logFatal = originLogFatalf }()
+	logs := []string{}
+	logFatal = func(format string, args ...interface{}) {
+		if len(args) > 0 {
+			logs = append(logs, fmt.Sprintf(format, args))
+		} else {
+			logs = append(logs, format)
+		}
+	}
+
+	type Test struct {
+		name              string
+		services          []string
+		before            func()
+		after             func()
+		expectedErrLength int
+	}
+
+	tests := []Test{
+		Test{
+			name:     "correctly validates good config",
+			services: []string{"biggraph"},
+			before: func() {
+				os.Setenv("services", "biggraph")
+				os.Setenv("biggraph_incoming_path", "/services/biggraph")
+				os.Setenv("biggraph_outgoing_url", "http://google.com")
+			},
+			after: func() {
+				os.Unsetenv("services")
+				os.Unsetenv("biggraph_incoming_path")
+				os.Unsetenv("biggraph_outgoing_url")
+			},
+			expectedErrLength: 0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			logs = []string{}
+			test.before()
+			cfg := readInConfig()
+			if test.expectedErrLength == 0 {
+				assert.Equal(t, logs, []string{})
+				assert.NotEqual(t, cfg, nil)
+			} else {
+				assert.Equal(t, len(logs), test.expectedErrLength)
+			}
+			test.after()
+		})
+	}
+}
