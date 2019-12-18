fa1218d63457ab0e62569fabb0bf59f86d323b89
dgoldstein1 <david.goldstein@deciphernow.com>
2019-12-18 12:31:57 -0500

    integration test is workinggit add -A

--- .circleci/integration_tests.sh
+++ .circleci/integration_tests.sh
@@ -7,13 +7,10 @@ set -e pipefail
 #########
 
 export PORT="9001"
-export services="google,wikipedia"
+export services="passthrough"
 
-export google_incoming_path="/google/"
-export google_outgoing_url="http://google.com"
-
-export wikipedia_incoming_path="/wikipedia/"
-export wikipedia_outgoing_url="http://wikipedia.org"
+export passthrough_incoming_path="/passthrough/"
+export passthrough_outgoing_url="http://localhost:9002/"
 
 ################
 ## run binary ##
@@ -26,20 +23,24 @@ pid=$!
 sleep 1
 cat proxy.log
 
+
+###############################
+## start passthrough service ##
+###############################
+
+
+ls .circleci/passthrough-service
+export PORT=9002
+.circleci/passthrough-service > passthrough.log 2>&1 &
+pid_passthrough=$!
+
+
 ###############
 ## run tests ##
 ###############
 
-
 > proxy.log
-URL="http://localhost:$PORT$google_incoming_path"
-echo "making request to: $URL"
-wget -O- $URL
-cat proxy.log
-
-
-> proxy.log
-URL="http://localhost:$PORT$wikipedia_incoming_path"
+URL="http://localhost:9001/passthrough/ping"
 echo "making request to: $URL"
 wget -O- $URL
 cat proxy.log
@@ -50,3 +51,6 @@ cat proxy.log
 
 echo "stopping reverse-proxy on pid $pid"
 kill $pid
+
+echo "stopping passthrough service on pid $pid_passthrough"
+kill $pid_passthrough
